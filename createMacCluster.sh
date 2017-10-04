0365dbeb381e363e5cf4504b0fb455caa2556913
Steven Glenn Jacobs <sjaco002@ucr.edu>
2017-10-04 10:24:08 -0700

    Added files that create my cluster locally (wont work for other users)

---
+++ createMacCluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/asterix/data_generator
+rm -rf asterix-mgmt
+mkdir asterix-mgmt
+cd asterix-mgmt
+unzip ~/asterix/asertixdb/asterixdb/asterix-installer/target/asterix-installer-0.9.3-SNAPSHOT-binary-assembly.zip
+rm conf/asterix-configuration.xml
+cp ../asterix-configuration.xml conf/
+./bin/managix create -n local -c clusters/local/local.xml
+cd ~/asterix/asertixdb
