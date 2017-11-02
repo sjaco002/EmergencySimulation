cb519d533eb21dd906d1e6c3d1e529f4ed086fe3
Steven Glenn Jacobs <sjaco002@ucr.edu>
2017-11-02 13:26:07 -0700

    Added two files

---
+++ scriptsAndResults/sendIndividualSubscriptions.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+
+COUNTER=420
+while [  $COUNTER -lt 421 ]; do
+  SUBCOUNTER=0
+  while [ $SUBCOUNTER -lt 100 ]; do
+    curl -G -H "Accept: application/x-adm" -v -d 'mode=asynchronous' --data-urlencode 'aql=use steven;
+    subscribe to EmergencyChannel ("p'"$COUNTER"'u'"$SUBCOUNTER"'") on brokerA;
+    ' http://promethium.ics.uci.edu:19002/sqlpp > reponses/reponses.txt
+    let SUBCOUNTER=SUBCOUNTER+1 
+  done
+  let COUNTER=COUNTER+1 
+done
+'''
+while [  $COUNTER -lt 1670 ]; do
+  SUBCOUNTER=0
+  while [ $SUBCOUNTER -lt 100 ]; do
+    curl -G -H "Accept: application/x-adm" -v -d 'mode=asynchronous' --data-urlencode 'aql=use steven;
+    subscribe to EmergencyChannel ("c'"$COUNTER"'u'"$SUBCOUNTER"'") on brokerA;
+    ' http://promethium.ics.uci.edu:19002/sqlpp > reponses/reponses.txt
+    let SUBCOUNTER=SUBCOUNTER+1 
+  done
+  let COUNTER=COUNTER+1 
+done
+
+while [  $COUNTER -lt 2505 ]; do
+  SUBCOUNTER=0
+  while [ $SUBCOUNTER -lt 100 ]; do
+    curl -G -H "Accept: application/x-adm" -v -d 'mode=asynchronous' --data-urlencode 'aql=use steven;
+    subscribe to EmergencyChannel ("w'"$COUNTER"'u'"$SUBCOUNTER"'") on brokerA;
+    ' http://promethium.ics.uci.edu:19002/sqlpp > reponses/reponses.txt
+    let SUBCOUNTER=SUBCOUNTER+1 
+  done
+  let COUNTER=COUNTER+1 
+done
+
+while [  $COUNTER -lt 5010 ]; do
+  SUBCOUNTER=0
+  while [ $SUBCOUNTER -lt 100 ]; do
+    curl -G -H "Accept: application/x-adm" -v -d 'mode=asynchronous' --data-urlencode 'aql=use steven;
+    subscribe to EmergencyChannel ("t'"$COUNTER"'u'"$SUBCOUNTER"'") on brokerA;
+    ' http://promethium.ics.uci.edu:19002/sqlpp > reponses/reponses.txt
+    let SUBCOUNTER=SUBCOUNTER+1 
+  done
+  let COUNTER=COUNTER+1 
+done
+'''
