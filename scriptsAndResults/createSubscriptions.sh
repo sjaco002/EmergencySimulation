0e95c52d8d6c10203cee64bbca996bc88ac7c043
Steven Glenn Jacobs <sjaco002@ucr.edu>
2017-11-14 16:06:57 -0800

    Updates

--- scriptsAndResults/createSubscriptions.sh
+++ scriptsAndResults/createSubscriptions.sh
@@ -2,38 +2,47 @@
 
 rm subscriptions.adm
 
-COUNTER=0
-while [  $COUNTER -lt 835 ]; do
+COUNTER=4
+while [  $COUNTER -lt 5 ]; do
   SUBCOUNTER=0
   while [ $SUBCOUNTER -lt 100 ]; do
-    echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "p'"$COUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
-    let SUBCOUNTER=SUBCOUNTER+1 
-  done
-  let COUNTER=COUNTER+1 
-done
-while [  $COUNTER -lt 1670 ]; do
-  SUBCOUNTER=0
-  while [ $SUBCOUNTER -lt 100 ]; do
-    echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "c'"$COUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
-    let SUBCOUNTER=SUBCOUNTER+1 
-  done
-  let COUNTER=COUNTER+1 
-done
-
-while [  $COUNTER -lt 2505 ]; do
-  SUBCOUNTER=0
-  while [ $SUBCOUNTER -lt 100 ]; do
-    echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "w'"$COUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
+    SUBSUBCOUNTER=0
+    while [ $SUBSUBCOUNTER -lt 10 ]; do
+      SUBSUBSUBCOUNTER=0
+      while [ $SUBSUBSUBCOUNTER -lt 10 ]; do
+        echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "p'"$COUNTER"''"$SUBSUBCOUNTER"''"$SUBSUBSUBCOUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
+        let SUBSUBSUBCOUNTER=SUBSUBSUBCOUNTER+1 
+      done
+      let SUBSUBCOUNTER=SUBSUBCOUNTER+1 
+    done
     let SUBCOUNTER=SUBCOUNTER+1 
   done
   let COUNTER=COUNTER+1 
 done
 
-while [  $COUNTER -lt 5010 ]; do
-  SUBCOUNTER=0
-  while [ $SUBCOUNTER -lt 100 ]; do
-    echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "t'"$COUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
-    let SUBCOUNTER=SUBCOUNTER+1 
-  done
-  let COUNTER=COUNTER+1 
-done
+#while [  $COUNTER -lt 1670 ]; do
+#  SUBCOUNTER=0
+#  while [ $SUBCOUNTER -lt 100 ]; do
+#    echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "c'"$COUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
+#    let SUBCOUNTER=SUBCOUNTER+1 
+#  done
+#  let COUNTER=COUNTER+1 
+#done#
+
+#while [  $COUNTER -lt 2505 ]; do
+#  SUBCOUNTER=0
+#  while [ $SUBCOUNTER -lt 100 ]; do
+#    echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "w'"$COUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
+#    let SUBCOUNTER=SUBCOUNTER+1 
+#  done
+#  let COUNTER=COUNTER+1 
+#done#
+
+#while [  $COUNTER -lt 5010 ]; do
+#  SUBCOUNTER=0
+#  while [ $SUBCOUNTER -lt 100 ]; do
+#    echo '{"DataverseName" : "steven" , "BrokerName" : "brokerA", "param0" : "t'"$COUNTER"'u'"$SUBCOUNTER"'" }' >> subscriptions.adm
+#    let SUBCOUNTER=SUBCOUNTER+1 
+#  done
+#  let COUNTER=COUNTER+1 
+#done
--- scriptsAndResults/startPolling.sh
+++ scriptsAndResults/startPolling.sh
@@ -2,17 +2,17 @@
 rm responses/responses.txt
 
 #835 and 100 would capture all users for p
-COUNTER=400
-while [  $COUNTER -lt 410 ]; do
+COUNTER=420
+while [  $COUNTER -lt 421 ]; do
 	SUBCOUNTER=0
 	while [ $SUBCOUNTER -lt 100 ]; do
-		curl --silent -G -H "Accept: application/x-adm" -v -d 'mode=asynchronous' --data-urlencode 'aql=use steven;
-          SELECT r AS report, l.userName
+		curl --silent -G -H "Accept: application/x-adm" -v -d 'mode=synchronous' --data-urlencode 'aql=use steven;
+          SELECT m.r AS report, m.l.userName
           FROM 
+          (select * from 
           (select value r from EmergencyReports r where r.insert_time > current_datetime() - day_time_duration("PT10S")) r,
-          (select value l from UserLocations l where l.insert_time > current_datetime() - day_time_duration("PT10S")) l
-          where l.userName = "p'"$COUNTER"'u'"$SUBCOUNTER"'" 
-          and spatial_intersect(r.location,l.location);
+          UserLocations l where spatial_intersect(r.location,l.location)) m
+          where m.l.userName = "p'"$COUNTER"'u'"$SUBCOUNTER"'";
 		' http://promethium.ics.uci.edu:19002/sqlpp >> responses/responses.txt &
 		let SUBCOUNTER=SUBCOUNTER+1 
 	done
